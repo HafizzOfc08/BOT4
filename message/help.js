1b4b2c3a9aec9a37e0493ee60da07299a1a8622d
Putragans9 <96307077+putragans9@users.noreply.github.com>
2021-12-26 00:28:24 +0700

    Add files via upload

---
+++ message/help.js
@@ -0,0 +1,244 @@
+exports.gameMenu = (prefix) => {
+    return `
+ 🕹️ GAME-MENU 🕹️
+ 
+き⃟🦈️ ${prefix}slot
+き⃟🦈️ ${prefix}limitgame
+き⃟🦈️ ${prefix}gelud @tag
+き⃟🦈️ ${prefix}tictactoe @tag
+き⃟🦈️ ${prefix}siapaaku
+き⃟🦈️ ${prefix}family100
+き⃟🦈️ ${prefix}kuismath
+き⃟🦈️ ${prefix}asahotak
+き⃟🦈️ ${prefix}tebaklirik
+き⃟🦈️ ${prefix}tebaklagu
+き⃟🦈️ ${prefix}tebakkata
+き⃟🦈️ ${prefix}susunkata
+き⃟🦈️ ${prefix}kimiakuis
+き⃟🦈️ ${prefix}caklontong
+き⃟🦈️ ${prefix}tebakjenaka
+き⃟🦈️ ${prefix}tebakanime
+き⃟🦈️ ${prefix}tebaktebakan
+き⃟🦈️ ${prefix}tebakgambar
+き⃟🦈️ ${prefix}tebakbendera
+き⃟🦈️ ${prefix}suit *batu/kertas/gunting*
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+   # FUN-MENU
+
+き⃟🦈️ ${prefix}mining
+き⃟🦈️ ${prefix}togel
+き⃟🦈️ ${prefix}cekwatak
+き⃟🦈️ ${prefix}cekmati [nama]
+き⃟🦈️ ${prefix}wangy [nama]
+き⃟🦈️ ${prefix}citacita
+き⃟🦈️ ${prefix}toxic
+き⃟🦈️ ${prefix}truth
+き⃟🦈️ ${prefix}dare
+き⃟🦈️ ${prefix}apakah
+き⃟🦈️ ${prefix}bisakah
+き⃟🦈️ ${prefix}kapankah
+き⃟🦈️ ${prefix}rate
+き⃟🦈️ ${prefix}jadian
+き⃟🦈️ ${prefix}cantik
+き⃟🦈️ ${prefix}ganteng
+き⃟🦈️ ${prefix}beban
+き⃟🦈️ ${prefix}babi
+き⃟🦈️ ${prefix}cekganteng
+き⃟🦈️ ${prefix}cekcantik
+
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+  #  OTHER-MENU 
+
+き⃟🦈️ ${prefix}brainly *query*
+き⃟🦈️ ${prefix}shopee *product*
+き⃟🦈️ ${prefix}playstore *query*
+き⃟🦈️ ${prefix}ssweb *query*
+き⃟🦈️ ${prefix}google *query*
+き⃟🦈️ ${prefix}image *query*
+き⃟🦈️ ${prefix}pinterest *query*
+き⃟🦈️ ${prefix}nulis *teks*
+き⃟🦈️ ${prefix}iguser *ussername*
+き⃟🦈️ ${prefix}igstalk *username*
+き⃟🦈️ ${prefix}githubstalk *username*
+き⃟🦈️ ${prefix}tiktokstalk *ussername*
+き⃟🦈️ ${prefix}img2url *reply foto*
+き⃟🦈️ ${prefix}ytsearch *query*
+
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+  # STICKER-MENU
+
+き⃟🦈️ ${prefix}dadu
+き⃟🦈️ ${prefix}doge
+き⃟🦈️ ${prefix}toimg
+き⃟🦈️ ${prefix}patrick
+き⃟🦈️ ${prefix}garwgura
+き⃟🦈️ ${prefix}ttg *teks*
+き⃟🦈️ ${prefix}attp *teks*
+き⃟🦈️ ${prefix}stickeranime
+き⃟🦈️ ${prefix}semoji *emoji*
+き⃟🦈️ ${prefix}sticker *reply foto/video*
+き⃟🦈️ ${prefix}smeme *teks|teks*
+き⃟🦈️ ${prefix}swm *pack|author*
+き⃟🦈️ ${prefix}take *pack|author* 
+き⃟🦈️ ${prefix}tovideo *reply sgif*
+
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+  # WIBU-MENU
+
+き⃟🦈️ ${prefix}loli
+き⃟🦈️ ${prefix}manga
+き⃟🦈️ ${prefix}anime 
+き⃟🦈️ ${prefix}lolivideo
+き⃟🦈️ ${prefix}husbu
+き⃟🦈️ ${prefix}waifu
+き⃟🦈️ ${prefix}milf
+き⃟🦈️ ${prefix}neko
+き⃟🦈️ ${prefix}kanna
+き⃟🦈️ ${prefix}sagiri
+き⃟🦈️ ${prefix}hentai
+き⃟🦈️ ${prefix}cosplay
+き⃟🦈️ ${prefix}wallnime
+き⃟🦈️ ${prefix}kusonime
+き⃟🦈️ ${prefix}megumin
+き⃟🦈️ ${prefix}otakudesu
+き⃟🦈️ ${prefix}doujindesu
+き⃟🦈️ ${prefix}storyanime
+き⃟🦈️ ${prefix}nakanomiku
+き⃟🦈️ ${prefix}nakanonino
+き⃟🦈️ ${prefix}nakanoitsuki
+き⃟🦈️ ${prefix}otakuongoing
+き⃟🦈️ ${prefix}nhentai *code*
+き⃟🦈️ ${prefix}nekopoi *link*
+き⃟🦈️ ${prefix}nekopoi3d
+き⃟🦈️ ${prefix}nekopoicosplay
+き⃟🦈️ ${prefix}nekopoisearch
+
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+  # OWNER-MENU
+
+き⃟🦈️ ${prefix}bc *teks*
+き⃟🦈️ ${prefix}term
+き⃟🦈️ ${prefix}eval
+き⃟🦈️ ${prefix}reset
+き⃟🦈️ ${prefix}clearall
+き⃟🦈️ ${prefix}leaveall
+き⃟🦈️ ${prefix}join *link gc*
+き⃟🦈️ ${prefix}shutdown
+き⃟🦈️ ${prefix}getquoted
+き⃟🦈️ ${prefix}addupdate *fiturnya*
+き⃟🦈️ ${prefix}exif *nama|author*
+き⃟🦈️ ${prefix}sewa add/del *waktunya*
+き⃟🦈️ ${prefix}premium add @tag|nomor
+き⃟🦈️ ${prefix}premium del @tag|nomor
+
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+  # GROUP-MENU
+
+き⃟🦈️ ${prefix}groupsetting
+き⃟🦈️ ${prefix}afk *alasan*
+き⃟🦈️ ${prefix}ceksewa
+き⃟🦈️ ${prefix}kickall
+き⃟🦈️ ${prefix}infogrup
+き⃟🦈️ ${prefix}promote
+き⃟🦈️ ${prefix}demote
+き⃟🦈️ ${prefix}listonline
+き⃟🦈️ ${prefix}tagall *teks*
+き⃟🦈️ ${prefix}leave
+き⃟🦈️ ${prefix}kick *reply*
+き⃟🦈️ ${prefix}add *+62xxxxxx*
+き⃟🦈️ ${prefix}setgrupname
+き⃟🦈️ ${prefix}setppgrup
+き⃟🦈️ ${prefix}setdesc
+き⃟🦈️ ${prefix}sider *reply chat bot*
+き⃟🦈️ ${prefix}hidetag *teks/reply teks*
+,
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+  ⬇️DOWNLOAD-MENU⬇️
+
+き⃟🦈️ ${prefix}fb 
+き⃟🦈️ ${prefix}igdl 
+き⃟🦈️ ${prefix}igdl2 
+き⃟🦈️ ${prefix}twitter 
+き⃟🦈️ ${prefix}tiktok 
+き⃟🦈️ ${prefix}play 
+き⃟🦈️ ${prefix}ythd 
+き⃟🦈️ ${prefix}ytmp3 
+き⃟🦈️ ${prefix}ytmp4 
+き⃟🦈️ ${prefix}soundcloud 
+き⃟🦈️ ${prefix}tiktoknowm 
+き⃟🦈️ ${prefix}tiktokaudio
+き⃟🦈️ ${prefix}mediafire 
+き⃟🦈️ ${prefix}nhentaipdf *code*
+
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+
+# RULES BOT
+
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+4. Gpp sc gw jelek yg penting 
+Bot gw jalan awokawoak
+Jika sudah dipahami rules-nya, silakan ketik *#menu* untuk memulai!
+
+5. Gw Jujur Gw Suka Sama Lu (Tapi Boong)
+
+OWNER  PUTRA BOT:
+wa.me/6283866844016
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+  # INFO-MENU
+
+き⃟🦈️ ${prefix}update
+き⃟🦈️ ${prefix}level
+き⃟🦈️ ${prefix}rules
+き⃟🦈️ ${prefix}profile
+き⃟🦈️ ${prefix}waktu
+き⃟🦈️ ${prefix}botstat
+き⃟🦈️ ${prefix}sewabot
+き⃟🦈️ ${prefix}listsewa
+き⃟🦈️ ${prefix}owner
+き⃟🦈️ ${prefix}ping
+き⃟🦈️ ${prefix}runtime
+き⃟🦈️ ${prefix}donasi
+き⃟🦈️ ${prefix}leaderboard
+き⃟🦈️ ${prefix}cekpremium
+き⃟🦈️ ${prefix}listpremium
+き⃟🦈️ ${prefix}sourcecode
+き⃟🦈️ ${prefix}bugreport *keluhan*
+
+`
+} 
